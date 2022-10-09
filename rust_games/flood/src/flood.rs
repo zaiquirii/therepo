e8a59e8b4b4decd4dfc44c47703d67ca0f6f283a
Zachary Smith <zsmith0@gmail.com>
2022-10-10 00:49:00 +0100

    flood: basic flood still working, now with spawners

--- rust_games/flood/src/flood.rs
+++ rust_games/flood/src/flood.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
+use bevy::tasks::ComputeTaskPool;
+use bevy::utils::Duration;
 use bevy_prototype_lyon::prelude::*;
 
 use crate::grid::Grid;
@@ -8,6 +10,12 @@ struct FloodCell {
     ground_height: f32,
     flood_height: f32,
 }
+
+const OFFSETS: [(i32, i32); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+const FLOOD_VISCOSITY: f32 = 0.5;
+const FLOOD_GRID_WIDTH: usize = 80;
+const FLOOD_GRID_HEIGHT: usize = 45;
+const GRID_CELL_SIZE: f32 = 10.0;
 
 pub struct Flood {
     width: usize,
@@ -34,11 +42,51 @@ impl Flood {
         self.dst_grid.get_mut(x, y).flood_height = height;
     }
 
-    pub fn step(&mut self, delta: f32) {
-        // let working_grid = self.src_grid;
-        // self.src_grid = self.dst_grid;
+    pub fn add_flood(&mut self, x: usize, y: usize, additional_flood: f32) {
+        self.dst_grid.get_mut(x, y).flood_height += additional_flood;
+    }
 
-        // self.dst_grid = working_grid;
+    pub fn step(&mut self, delta: f32) {
+        std::mem::swap(&mut self.src_grid, &mut self.dst_grid);
+
+        /* Rules:
+        - Preserve amount of flood
+        - Naive approach for now please
+         */
+        for y in 0..self.height {
+            for x in 0..self.width {
+                /* For each neighbor:
+                - calculate transfer amount by
+                    - calculating height difference
+                    - apply viscosity and time delta
+                    - only update index in dst
+                    - will require double work for now (only check down and right?)
+                // SKIPPING ground height for now
+                 */
+
+                let source_tile = self.src_grid.get(x, y);
+                let mut new_height = source_tile.flood_height;
+                for (x_offset, y_offset) in OFFSETS {
+                    let x_target = x as i32 + x_offset;
+                    let y_target = y as i32 + y_offset;
+
+                    if x_target < 0
+                        || x_target as usize >= self.width
+                        || y_target < 0
+                        || y_target as usize >= self.height
+                    {
+                        continue;
+                    }
+
+                    let target_tile = self.src_grid.get(x_target as usize, y_target as usize);
+                    let height_difference = target_tile.flood_height - source_tile.flood_height;
+                    let change = height_difference * FLOOD_VISCOSITY * delta;
+                    new_height += change;
+                }
+
+                self.dst_grid.get_mut(x, y).flood_height = new_height;
+            }
+        }
     }
 }
 
@@ -52,35 +100,29 @@ pub struct FloodTile {
 pub struct FloodEnt;
 
 pub fn setup_flood_demo_system(mut commands: Commands) {
-    let size = 40;
-    let mut flood = Flood::new(size, size);
-    for i in 0..40 {
-        flood.set_flood_height(i, 5, i as f32 / 40.0);
-    }
+    let flood = Flood::new(FLOOD_GRID_WIDTH, FLOOD_GRID_HEIGHT);
     commands.insert_resource(flood);
 
-    let tile_size = 20.0;
     let tile_shape = shapes::Rectangle {
-        extents: Vec2::new(20.0, 20.0),
+        extents: Vec2::new(GRID_CELL_SIZE, GRID_CELL_SIZE),
         origin: RectangleOrigin::Center,
     };
 
-    for x in 0..size {
-        for y in 0..size {
+    for y in 0..FLOOD_GRID_HEIGHT {
+        for x in 0..FLOOD_GRID_WIDTH {
             commands
                 .spawn_bundle(GeometryBuilder::build_as(
                     &tile_shape,
                     DrawMode::Fill(FillMode::color(Color::WHITE)),
-                    // DrawMode::Fill(FillMode::color(Color::GREEN)),
-                    Transform::from_xyz(
-                        x as f32 * tile_size - 300.0,
-                        y as f32 * tile_size - 300.0,
-                        0.0,
-                    ),
+                    Transform::from_xyz(x as f32 * GRID_CELL_SIZE, y as f32 * GRID_CELL_SIZE, 0.0),
                 ))
                 .insert(FloodTile { x, y });
         }
     }
+
+    spawn_spawner(&mut commands, 0, 20, 10.0, 1.0);
+    spawn_spawner(&mut commands, 20, 30, 10.0, 1.0);
+    spawn_spawner(&mut commands, 0, 0, 30.0, 1.0);
 }
 
 pub fn update_flood_system(time: Res<Time>, mut flood: ResMut<Flood>) {
@@ -91,9 +133,54 @@ pub fn update_flood_render_system(
     flood: Res<Flood>,
     mut query: Query<(&FloodTile, &mut DrawMode)>,
 ) {
-    for (tile, mut draw_mode) in &mut query {
+    query.for_each_mut(|(tile, mut draw_mode)| {
         let flood_height = flood.get_flood_height(tile.x, tile.y);
         let color = Color::rgb(0.0, 0.0, flood_height);
         *draw_mode = DrawMode::Fill(FillMode::color(color));
+    });
+}
+
+#[derive(Component)]
+pub struct FloodSpawner {
+    discharge: f32,
+    period: f32,
+    timer: Timer,
+}
+
+#[derive(Component)]
+pub struct GridPosition {
+    x: usize,
+    y: usize,
+}
+
+fn spawn_spawner(commands: &mut Commands, x: usize, y: usize, discharge: f32, period: f32) {
+    let shape = shapes::Rectangle {
+        extents: Vec2::new(10.0, 10.0),
+        origin: RectangleOrigin::Center,
+    };
+
+    commands
+        .spawn_bundle(GeometryBuilder::build_as(
+            &shape,
+            DrawMode::Fill(FillMode::color(Color::GREEN)),
+            Transform::from_xyz(x as f32 * GRID_CELL_SIZE, y as f32 * GRID_CELL_SIZE, 0.0),
+        ))
+        .insert(GridPosition { x, y })
+        .insert(FloodSpawner {
+            discharge,
+            period,
+            timer: Timer::new(Duration::from_secs_f32(period), true),
+        });
+}
+
+pub fn spawner_discharge_flood_system(
+    time: Res<Time>,
+    mut flood: ResMut<Flood>,
+    mut query: Query<(&GridPosition, &mut FloodSpawner)>,
+) {
+    for (grid_position, mut spawner) in &mut query {
+        if spawner.timer.tick(time.delta()).just_finished() {
+            flood.add_flood(grid_position.x, grid_position.y, spawner.discharge);
+        }
     }
 }
--- rust_games/flood/src/main.rs
+++ rust_games/flood/src/main.rs
@@ -5,27 +5,45 @@ mod mobs;
 mod movement;
 mod towers;
 
+use std::time::Duration;
+
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
+use bevy::render::camera;
 use bevy_prototype_lyon::prelude::*;
-use flood::{setup_flood_demo_system, update_flood_render_system, update_flood_system};
+
+use flood::{
+    setup_flood_demo_system, spawner_discharge_flood_system, update_flood_render_system,
+    update_flood_system,
+};
+use iyes_loopless::prelude::*;
 use mobs::spawn_mob;
 use movement::movement_system;
 use towers::towers::{spawn_tower, target_towers_system};
 
-// use towers::towers::;
-
 fn main() {
+    let mut flood_fixed_update_stage = SystemStage::parallel();
+    flood_fixed_update_stage
+        .add_system(spawner_discharge_flood_system)
+        .add_system(update_flood_system)
+        .add_system(update_flood_render_system);
+
     App::new()
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_plugin(ShapePlugin)
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_stage_before(
+            CoreStage::Update,
+            "flood_fixed_update",
+            FixedTimestepStage::new(Duration::from_millis(33)).with_stage(flood_fixed_update_stage),
+        )
         .add_startup_system(setup_system)
         .add_startup_system(setup_flood_demo_system)
         .add_system(rotate_system)
         .add_system(movement_system)
         .add_system(target_towers_system)
-        .add_system(update_flood_system)
-        .add_system(update_flood_render_system)
         .run();
 }
 
@@ -33,7 +51,11 @@ fn main() {
 struct Rotate(f32);
 
 fn setup_system(mut commands: Commands) {
-    commands.spawn_bundle(Camera2dBundle::default());
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.transform = camera_bundle
+        .transform
+        .with_translation(Vec3::new(300.0, 300.0, 0.0));
+    commands.spawn_bundle(camera_bundle);
     spawn_tower(&mut commands, Vec2::new(0.0, 0.0));
     spawn_tower(&mut commands, Vec2::new(20.0, 20.0));
     spawn_tower(&mut commands, Vec2::new(-30.0, -100.0));
